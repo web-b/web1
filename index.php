6e67a198643d1572eea50830ced181811737e130
web-b <47978540+web-b@users.noreply.github.com>
2019-03-01 13:26:27 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,179 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+<title>r과정중심평가테스</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css.css">
+<link type="text/css" rel="stylesheet" href="style.css"/>    
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script> 
+    <script src="jquery.min.js"></script>
+    
+<style>
+h1,h2,h3,h4,h5,h6 {font-family: "Oswald"}
+body {font-family: "Open Sans"}
+    
+    
+.select-boxes{width: 280px;text-align: center;}
+select {
+    background-color: #F5F5F5;
+    border: 1px double #15a6c7;
+    color: #1d93d1;
+    font-family: Georgia;
+    font-weight: bold;
+    font-size: 14px;
+    height: 39px;
+    padding: 7px 8px;
+    width: 250px;
+    outline: none;
+    margin: 10px 0 10px 0;
+}
+select option{
+    font-family: Georgia;
+    font-size: 14px;
+}
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+</style>
+    <script type="text/javascript">
+$(document).ready(function(){
+    $('#country').on('change',function(){
+        var countryID = $(this).val();
+        if(countryID){
+            $.ajax({
+                type:'POST',
+                url:'ajaxData.php',
+                data:'country_id='+countryID,
+                success:function(html){
+                    $('#state').html(html);
+                    $('#city').html('<option value="">과목 선택 먼저하세요.</option>'); 
+                }
+            }); 
+        }else{
+            $('#state').html('<option value="">학년 선택 먼저하세요.</option>');
+            $('#city').html('<option value="">과목 선택 먼저하세요.</option>'); 
+        }
+    });
+    
+    $('#state').on('change',function(){
+        var stateID = $(this).val();
+        if(stateID){
+            $.ajax({
+                type:'POST',
+                url:'ajaxData.php',
+                data:'state_id='+stateID,
+                success:function(html){
+                    $('#city').html(html);
+                }
+            }); 
+        }else{
+            $('#city').html('<option value="">Select state first</option>'); 
+        }
+    });
+});
+</script>
+      
+    
+<body class="w3-light-grey">
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<!-- Navigation bar with social media icons -->
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1600px">
+
+  <!-- Header -->
+
+  </header>
+  
+  <!-- Image header -->
+ 
+
+  <!-- Grid -->
+  <div class="w3-row w3-padding w3-border">
+
+    <!-- Blog entries -->
+    <div class="w3-col l12 s12">
+    
+      <!-- Blog entry -->
+      <div class="w3-container w3-white w3-margin w3-padding-large">
+        
+          <h2 style="text-align: center";>과정중심 평가 테스트 사이트</h2>
+          <br>
+          <div class="select-boxes">
+    <?php
+    //Include database configuration file
+    include('dbConfig.php');
+    
+    //Get all country data
+    $query = $db->query("SELECT * FROM countries WHERE status = 1 ORDER BY country_name ASC");
+    
+    //Count total number of rows
+    $rowCount = $query->num_rows;
+    ?>
+     <h2>학년</h2>         
+    <select name="country" id="country" >
+        <option value="">학년</option>
+        <?php
+        if($rowCount > 0){
+            while($row = $query->fetch_assoc()){ 
+                echo '<option value="'.$row['country_id'].'">'.$row['country_name'].'</option>';
+            }
+        }else{
+            echo '<option value="">Country not available</option>';
+        }
+        ?>
+    </select>
+              
+    <h2>과목</h2>
+              
+    <select name="state" id="state">
+        <option value="">학년 선택 먼저하세요.</option>
+    </select>
+              
+     <h2>평가기준</h2>
+    
+    <select name="city" id="city">
+        <option value="">과목 선택 먼저하세요.</option>
+    </select>
+     
+       <h2>평가결과</h2>    
+              
+     <select name="color" id="color">
+        <option value="">평가기준 선택 먼저하세요.</option>
+    </select>
+              
+    </div>
+
+          
+            
+      </div>
+      
+    </div>
+
+  </div>
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
